4ef62225e3142184aa3bad02129f992e17ed33a7
Sergi Tur Badenas <sergitur@ebretic.com>
2013-08-20 14:12:19 +0200

    Create catalan.php

---
+++ assets/image_crud/languages/catalan.php
@@ -0,0 +1,16 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$lang['upload_button'] = 'Pugueu els fitxers aquí';
+$lang['upload-drop-area'] = 'Deixeu aquí (drop) els fitxers per a pujar-los';
+$lang['upload-cancel'] = 'Cancel·lar';
+$lang['upload-failed'] = 'Fallat';
+
+$lang['loading'] = 'Carregant, si us plau espereu...';
+$lang['deleting'] = 'Esborrant, si us plau espereu...';
+$lang['saving_title'] = 'Salvant el títol...';
+
+$lang['list_delete'] = 'Esborrar';
+$lang['alert_delete'] = 'Esteu segurs que voleu esborrar la imatge?';
+
+/* End of file english.php */
+/* Location: ./assets/image_crud/languages/english.php */
